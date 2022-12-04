e217d80d46cbde79abcc15046797ba2646c84bbd
chloeseo0210 <chloeseo0210@gmail.com>
2022-12-05 01:24:27 +0900

    feat : add Authmodule in app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { BoardModule } from './board/board.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UserModule, BoardModule],
+  imports: [UserModule, BoardModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
