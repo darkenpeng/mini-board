4d5ea537b2405dbf123866dd5ec78f1ba91af7c0
chloeseo0210 <chloeseo0210@gmail.com>
2022-12-05 01:25:52 +0900

    feat : e2e test only using status code

--- backend/test/app.e2e-spec.ts
+++ backend/test/app.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -20,5 +20,49 @@ describe('AppController (e2e)', () => {
       .get('/')
       .expect(200)
       .expect('Hello World!');
+  });
+
+  it('/user (GET)', () => {
+    return request(app.getHttpServer()).get('/user').expect(200); //.expect('');
+  });
+
+  it('/user (PATCH)', () => {
+    return request(app.getHttpServer()).patch('/user').expect(200); //.expect('');
+  });
+
+  it('/user/withdraw (PATCH)', () => {
+    return request(app.getHttpServer()).patch('/user/withdraw').expect(200); //.expect('');
+  });
+
+  it('/register (POST)', () => {
+    return request(app.getHttpServer()).post('/register').expect(201); //.expect('');
+  });
+
+  it('/login (POST)', () => {
+    return request(app.getHttpServer()).post('/login').expect(201); //.expect('');
+  });
+
+  it('/board (POST)', () => {
+    return request(app.getHttpServer()).post('/board').expect(201); //.expect('');
+  });
+
+  it('/board (GET)', () => {
+    return request(app.getHttpServer()).get('/board').expect(200); //.expect('');
+  });
+
+  it('/board/{id} (GET)', () => {
+    return request(app.getHttpServer()).get('/board/1').expect(200); //.expect('');
+  });
+
+  it('/board/{id} (PATCH)', () => {
+    return request(app.getHttpServer()).patch('/board/1').expect(200); //.expect('');
+  });
+
+  it('/board/{id} (DELETE)', () => {
+    return request(app.getHttpServer()).delete('/board/1').expect(204); //.expect('');
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
