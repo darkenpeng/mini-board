271aa003cb5d644e3ae2cdd92ee5f33cc00a3b24
chloeseo0210 <chloeseo0210@gmail.com>
2022-12-04 17:01:43 +0900

    feat : add modules to link swagger

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { BoardModule } from './board/board.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, BoardModule],
   controllers: [AppController],
   providers: [AppService],
 })
