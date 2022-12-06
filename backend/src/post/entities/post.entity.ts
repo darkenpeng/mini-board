4760a0afb2a5895ffdb358d4b3e877a8c055e5ed
chloeseo0210 <chloeseo0210@gmail.com>
2022-12-06 22:15:17 +0900

    feat : implement postEntity using typeorm

--- backend/src/post/entities/post.entity.ts
+++ backend/src/post/entities/post.entity.ts
@@ -1,10 +1,32 @@
-export class PostEntity {
+import {
+  Entity,
+  Column,
+  PrimaryColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Post {
+  @PrimaryColumn()
   slug: string;
+
+  @Column({
+    length: 32, //TODO
+  })
   title: string;
+
+  @Column({
+    type: 'text',
+    length: 32648,
+  })
   content: string;
   //author: User;
   //favorited: boolean;
-  //favoritesCount?: number;
+  //favoritesCount: number;
+  @CreateDateColumn()
   createdAt: Date;
+
+  @UpdateDateColumn()
   updatedAt: Date;
 }
