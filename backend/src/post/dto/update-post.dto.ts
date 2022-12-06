d8adc1ad86da56f8dea0599b503e0310f3d56440
chloeseo0210 <chloeseo0210@gmail.com>
2022-12-06 22:23:12 +0900

    fix : delete update-postDto

--- backend/src/post/dto/update-post.dto.ts
+++ backend/src/post/dto/update-post.dto.ts
@@ -1,17 +1,5 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreatePostDto } from './create-post.dto';
-import { IsNotEmpty, IsString } from 'class-validator';
+//import { IsNotEmpty, IsString } from 'class-validator';
 
-export class UpdatePostDto extends PartialType(CreatePostDto) {
-  @IsNotEmpty()
-  @IsString()
-  title: string;
-
-  @IsNotEmpty()
-  @IsString()
-  content: string;
-
-  @IsNotEmpty()
-  @IsString()
-  slug: string;
-}
+export class UpdatePostDto extends PartialType(CreatePostDto) {}
