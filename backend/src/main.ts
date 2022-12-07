5b329b47b59d21b412ca7714870bff26e4ae1db4
chloeseo0210 <chloeseo0210@gmail.com>
2022-12-07 18:56:49 +0900

    fix : resolve cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,9 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
 
   const config = new DocumentBuilder()
     .setTitle('User example')
