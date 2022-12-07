4e03e8c1420d1aeb854196e1b6d360d0bf41ac00
chloeseo0210 <chloeseo0210@gmail.com>
2022-12-07 19:02:28 +0900

    fix : add provider

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,29 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
 import { AuthModule } from './auth/auth.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Post } from './post/entities/post.entity';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [UserModule, PostModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.host,
+      port: parseInt(process.env.port),
+      username: process.env.username,
+      password: process.env.password,
+      database: 'postgres',
+      synchronize: true,
+      logging: true,
+      entities: [Post],
+      subscribers: [],
+      migrations: [],
+    }),
+    UserModule,
+    PostModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/post/post.module.ts
+++ backend/src/post/post.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { PostService } from './post.service';
 import { PostController } from './post.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Post } from './entities/post.entity';
+import { PostTypeOrmRepository } from './post.respository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Post])],
   controllers: [PostController],
-  providers: [PostService],
+  providers: [
+    {
+      provide: 'POST_REPOSITORY',
+      useClass: PostTypeOrmRepository,
+    },
+    PostService,
+  ],
 })
 export class PostModule {}
