8f2fa11792933fffb80512238389ae2924574ae2
chloeseo0210 <chloeseo0210@gmail.com>
2022-12-08 18:16:19 +0900

    feat : add decorator

--- backend/src/post/dto/create-post.dto.ts
+++ backend/src/post/dto/create-post.dto.ts
@@ -1,10 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 //https://docs.nestjs.com/techniques/validation#using-the-built-in-validationpipe
 export class CreatePostDto {
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   title: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   content: string;
