5a7abcf30e05b55085197e8c39de1b1d2dff0f47
gup97 <gup97@naver.com>
2022-12-09 21:56:41 +0900

    fix : change naming
    userModel -> userRepository

--- backend/src/users/users.repository.ts
+++ backend/src/users/users.repository.ts
@@ -9,19 +9,19 @@ import { UserDto } from './dtos';
 export class UserTypeOrmRepository {
   constructor(
     @InjectRepository(UserEntity)
-    private userModel: Repository<UserEntity>,
+    private userRepository: Repository<UserEntity>,
   ) {}
 
   async create(newUser: UserDto): Promise<UserEntity> {
-    const createdUser = await this.userModel.save(newUser);
+    const createdUser = await this.userRepository.save(newUser);
     return createdUser;
   }
   async getAll(): Promise<UserEntity[]> {
-    return await this.userModel.find();
+    return await this.userRepository.find();
   }
   // 내 생각엔 Dto 형식보다 프로퍼티가 3개많아서 에러가 떠야하는데 정상작동함...
   async getOneByEmail(email: string): Promise<UserEntity> {
-    const test = await this.userModel.findOneBy({ email });
+    const test = await this.userRepository.findOneBy({ email });
     return test;
   }
 
@@ -29,11 +29,11 @@ export class UserTypeOrmRepository {
     email: string,
     updateUserDto: UpdateUserDto,
   ): Promise<void> {
-    await this.userModel.update({ email: email }, updateUserDto);
+    await this.userRepository.update({ email: email }, updateUserDto);
   }
 
   async deleteByEmail(email: string): Promise<DeleteResult> {
-    const result = await this.userModel.delete({ email });
+    const result = await this.userRepository.delete({ email });
     return result;
   }
 }
